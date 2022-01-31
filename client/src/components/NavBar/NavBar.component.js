55f85b1e1e1e628dd40c4880d93d62d29e048c49
H <hsilvap@me.com>
2022-01-30 20:04:06 -0600

    fix navigation

--- client/src/components/NavBar/NavBar.component.js
+++ client/src/components/NavBar/NavBar.component.js
@@ -1,13 +1,12 @@
 import * as React from 'react'
-import { Navigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { StyledNavBar } from './NavBar.styled'
 import { SearchBar } from '../SearchBar/SearchBar.component'
 import logo from '../../assets/logo.png' 
 
 export const NavBar = () => {
-    const onClickLogo = () => {
-        <Navigate to="/"/>
-    }
+    let navigate = useNavigate();
+    const onClickLogo = () => (navigate("/"))
     return <StyledNavBar>
         <img src={logo} alt='logo' onClick={onClickLogo} />
         <SearchBar/>
